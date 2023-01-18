c945b27e751cfb2bb390b50f50cf747dedacf03f
Maksym <m.pravdin@icloud.com>
2023-01-18 22:10:18 +0300

    Added edits for popup and add js to buttons

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let popupBlock = document.querySelector('.popup-wrapper--closed')
+let popupButton = document.querySelector('.add-to-cart')
+let popupButtonAdd = document.querySelector('.popup__button')
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -12,3 +15,13 @@ navToggle.addEventListener('click', function() {
       navMain.classList.remove('main-nav--opened');
     }
 });
+
+if (popupButton) {
+  popupButton.addEventListener('click', function() {
+    if (popupBlock.classList.contains('popup-wrapper--closed')) {
+      popupBlock.classList.remove('popup-wrapper--closed');
+      popupBlock.classList.add('popup-wrapper--opened');
+      }
+  });
+}
+
